3b827a340d0084648ed35fbc66547308f1ce1405
syedmareeha <syedmareeha0607@gmail.com>
2025-07-18 13:54:20 +0530

    Update products.js

--- products.js
+++ products.js
@@ -4,13 +4,13 @@ const foodItems = {
       id: "p1",
       name: "Margherita Pizza",
       price: 250,
-      image: "assets/images/margherita.jpeg"
+      image: "margherita.jpeg"
     },
     {
       id: "p2",
       name: "Pepperoni Pizza",
       price: 300,
-      image: "assets/images/papperoni.jpeg"
+      image: "papperoni.jpeg"
     }
   ],
   burger: [
@@ -18,13 +18,13 @@ const foodItems = {
       id: "b1",
       name: "Cheeseburger",
       price: 180,
-      image: "assets/images/cheese_burger.jpeg"
+      image: "cheese_burger.jpeg"
     },
     {
       id: "b2",
       name: "Veggie Burger",
       price: 150,
-      image: "assets/images/veggie_burger.jpeg"
+      image: "veggie_burger.jpeg"
     }
   ],
   dessert: [
@@ -32,13 +32,13 @@ const foodItems = {
       id: "d1",
       name: "Chocolate Cake",
       price: 120,
-      image: "assets/images/chocolate_cake.jpeg"
+      image: "chocolate_cake.jpeg"
     },
     {
       id: "d2",
       name: "Ice Cream Sundae",
       price: 100,
-      image: "assets/images/ice_cream.jpeg"
+      image: "ice_cream.jpeg"
     }
   ]
 };
